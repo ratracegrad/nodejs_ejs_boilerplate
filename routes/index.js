ec6f095862e9aa9d019a452bbccfd93ee5062d63
Jennifer Bland <ratracegrad@gmail.com>
2016-04-02 17:41:51 -0400

    updated to add items that were missing with jslint

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
-'use strict'
+'use strict';
 
 module.exports = function(app) {
     app.get('/', function(req, res) {
-        res.render('pages/index')
+        res.render('pages/index');
     });
 
     app.get('/about', function(req, res) {
         res.render('pages/about');
     });
-}
+};
