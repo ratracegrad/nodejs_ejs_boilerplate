ed5ac6c7ca943933b2d80eaa30f0e1c15231a3c5
Jennifer Bland <ratracegrad@gmail.com>
2016-04-02 16:08:05 -0400

    Removed serve-favicon and path to load favicon image

--- server.js
+++ server.js
@@ -4,8 +4,6 @@
 // get all the tools we need
 // ================================================================
 var express = require('express');
-var favicon = require('serve-favicon');
-var path = require('path');
 var routes = require('./routes/index.js');
 var port = process.env.PORT || 3000;
 
@@ -16,7 +14,7 @@ var app = express();
 // ================================================================
 app.use('/public', express.static(process.cwd() + '/public'));
 app.set('view engine', 'ejs');
-app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
+
 
 // ================================================================
 // setup routes
